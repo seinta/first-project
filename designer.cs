a995b8fba646e1fe835860314d937518df3cac64
seinta <seintaridis@outlook.com>
2013-09-09 12:07:45 +0300

    Update designer.cs

--- designer.cs
+++ designer.cs
@@ -1,4 +1,5 @@
-namespace WindowsFormsApplication15
+
+ namespace WindowsFormsApplication15
 {
     partial class Form1
     {
@@ -36,13 +37,11 @@ namespace WindowsFormsApplication15
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(498, 3);
+            this.textBox1.Location = new System.Drawing.Point(252, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -59,9 +58,9 @@ namespace WindowsFormsApplication15
             this.columnHeader6,
             this.columnHeader7});
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(323, 48);
+            this.listView1.Location = new System.Drawing.Point(3, 38);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(449, 186);
+            this.listView1.Size = new System.Drawing.Size(634, 581);
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -70,58 +69,38 @@ namespace WindowsFormsApplication15
             // columnHeader1
             // 
             this.columnHeader1.Text = "Name/Surname";
-            this.columnHeader1.Width = 99;
+            this.columnHeader1.Width = 128;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "Time-In";
-            this.columnHeader3.Width = 78;
+            this.columnHeader3.Width = 118;
             // 
             // columnHeader4
             // 
             this.columnHeader4.Text = "Work time";
-            this.columnHeader4.Width = 79;
+            this.columnHeader4.Width = 128;
             // 
             // columnHeader5
             // 
             this.columnHeader5.Text = "Break time";
-            this.columnHeader5.Width = 69;
+            this.columnHeader5.Width = 102;
             // 
             // columnHeader6
             // 
             this.columnHeader6.Text = "Time-Out";
+            this.columnHeader6.Width = 108;
             // 
             // columnHeader7
             // 
             this.columnHeader7.Text = "Break";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(308, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(705, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.columnHeader7.Width = 92;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1015, 284);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(640, 631);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -142,8 +121,6 @@ namespace WindowsFormsApplication15
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ColumnHeader columnHeader6;
         private System.Windows.Forms.ColumnHeader columnHeader7;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
